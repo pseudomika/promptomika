a6a6df5ce513ed103969afa4c235724550f94406
pseudomika <japan.avenir@gmail.com>
2025-02-28 18:48:35 +0800

    Add authentication

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Auth elements
+    const authOverlay = document.getElementById("auth-overlay")
+    const authUsername = document.getElementById("username")
+    const authPassword = document.getElementById("password")
+    const loginButton = document.getElementById("login")
     // Search bar elements
     const tagsContainer = document.getElementById("tags-container");
     const tagInput = document.getElementById("tag-input");
@@ -41,10 +46,44 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    function OpenInNewTab(url) {
+    function openInNewTab(url) {
         var win = window.open(url, '_blank');
         win.focus();
     }
+
+    function hashCode(s) {
+        return s.split("").reduce(function (a, b) {
+            a = ((a << 5) - a) + b.charCodeAt(0);
+            return a & a;
+        }, 0);
+    }
+
+    function login() {
+        const username = authUsername.value;
+        const password = authPassword.value;
+        if (!username || !password) return;
+        if (hashCode(username) === 1424168624 && hashCode(password) === -935982680) {
+            authOverlay.style.display = "none"
+        };
+    }
+
+    loginButton.addEventListener("click", function () {
+        login();
+    })
+
+    authUsername.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            e.preventDefault()
+            login()
+        };
+    })
+
+    authPassword.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            e.preventDefault()
+            login()
+        };
+    })
 
     // Manually input new tag, click space or tab to complete
     tagInput.addEventListener("keydown", function (e) {
@@ -55,6 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    // Click reset button to remove all keywords from search bar
     resetButton.addEventListener("click", function () {
         const tags = [...tagsContainer.querySelectorAll(".tag")]
         for (const tag of tags) {
@@ -65,6 +105,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 
+    // Click submit button to view result
     submitButton.addEventListener("click", function () {
         const tags = [...tagsContainer.querySelectorAll(".tag")].map(tag => tag.textContent.replace("×", "").trim());
 
@@ -72,12 +113,13 @@ document.addEventListener("DOMContentLoaded", function () {
             const baseUrl = "https://jable.tv/search"
             const url = `${baseUrl}/${tags.join("-")}/`
             console.log(`Generated URL: ${url}`)
-            OpenInNewTab(url)
+            openInNewTab(url)
         } else {
             console.warn("Search box is empty")
         }
     });
 
+    // Add keyword when clicked
     for (const elem of suggestions) {
         elem.addEventListener("click", function (e) {
             if (e.target.classList.contains("suggestion")) {
